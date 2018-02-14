902e1cebd6250c98b4da1a809600535dba108778
Ean Bowman <eanbowman@gmail.com>
2018-02-14 14:27:01 -0500

    Initial commit. The whole shebang!

---
+++ Assets/DialogueBoxHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBoxHandler : MonoBehaviour {
+    public GameObject panelInstance;
+
+    public void Hide()
+    {
+        if (panelInstance)
+        {
+            panelInstance.SetActive(false);
+        } else
+        {
+            Debug.LogError("panelInstance is not set! Can't deactivate non-existant panel! The variable should point to the panel which contains this button.");
+        }
+    }
+}
